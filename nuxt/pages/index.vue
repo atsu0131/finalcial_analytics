e7a030f4d172fe95756adab16fe240adc9025f6d
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-07-04 17:56:55 +0900

    first commit

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <NuxtLogo />
+      <h1 class="title">
+         {{ greet }}
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ app }): Promise<object> {
+    const greet: string = await app.$axios.$get('/').catch((err) => err)
+    return { greet }
+  }
+})
+</script>
