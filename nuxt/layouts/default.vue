7338e1d732baf9f7c333deef17da8d0f835751af
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-07-18 18:11:38 +0900

    add

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav-bar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/layouts/NavBar.vue";
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
