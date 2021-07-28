c114f6e6164dc01eb3e5cbec31cce84ece74ede7
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-07-28 23:08:13 +0900

    add

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -29,6 +29,9 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
